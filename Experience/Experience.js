5af1fce694ac17fefb2ddbe936bf2690bd984216
chiggy69 <chiragaggarwal69@gmail.com>
2022-11-23 22:33:33 +0530

    Event captured and instance updated

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,8 +1,12 @@
 import * as THREE from "three";
+//utils
 import Sizes from "./Utils/Sizes";
+import Time from "./Utils/Time";
 
 import Camera from "./Camera";
 import Renderer from "./Renderer";
+
+import World from "./World/World";
 export default class Experience {
 	static instance;
 	constructor(canvas) {
@@ -14,8 +18,32 @@ export default class Experience {
 		Experience.instance = this;
 		this.canvas = canvas;
 		this.scene = new THREE.Scene();
+		this.time = new Time();
 		this.sizes = new Sizes();
 		this.camera = new Camera();
 		this.renderer = new Renderer();
+
+		this.world = new World();
+
+		//captures event tick dispatched by time
+		this.time.on("tick", () => {
+			//update all other classes
+			this.update();
+		});
+
+		//captures event resize dispatched by Sizes
+		this.sizes.on("resize", () => {
+			this.resize();
+		});
+	}
+
+	update() {
+		this.camera.update();
+		this.renderer.update();
+	}
+
+	resize() {
+		this.camera.resize();
+		this.renderer.resize();
 	}
 }
