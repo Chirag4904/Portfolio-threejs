d13b3c46eb05c74239516999cff59601557bcadd
chiggy69 <chiragaggarwal69@gmail.com>
2022-11-24 00:29:45 +0530

    Load resources class created

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -2,11 +2,14 @@ import * as THREE from "three";
 //utils
 import Sizes from "./Utils/Sizes";
 import Time from "./Utils/Time";
+import Resources from "./Utils/Resources";
+import assets from "./Utils/assets";
 
 import Camera from "./Camera";
 import Renderer from "./Renderer";
 
 import World from "./World/World";
+
 export default class Experience {
 	static instance;
 	constructor(canvas) {
@@ -22,8 +25,12 @@ export default class Experience {
 		this.sizes = new Sizes();
 		this.camera = new Camera();
 		this.renderer = new Renderer();
-
+		this.resources = new Resources(assets);
 		this.world = new World();
+
+		this.resources.on("loaded", () => {
+			console.log("hello");
+		});
 
 		//captures event tick dispatched by time
 		this.time.on("tick", () => {
--- Experience/World/World.js
+++ Experience/World/World.js
@@ -5,7 +5,10 @@ export default class World {
 	constructor() {
 		this.experience = new Experience();
 		this.scene = this.experience.scene;
+		this.resources = this.experience.resources;
 
-		this.room = new Room();
+		this.resources.on("loaded", () => {
+			this.room = new Room();
+		});
 	}
 }
