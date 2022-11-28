9bffbe28fd27a6c6d78bbe6dbfa22a1d14de4f6a
chiggy69 <chiragaggarwal69@gmail.com>
2022-11-28 17:02:20 +0530

    responsive gsap scrollTrigger added

--- Experience/World/Controls.js
+++ Experience/World/Controls.js
@@ -1,13 +1,20 @@
 import * as THREE from "three";
-import gsap from "gsap";
+import GSAP from "gsap";
 import Experience from "../Experience";
+
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 export default class Controls {
 	constructor() {
 		this.experience = new Experience();
 		this.scene = this.experience.scene;
+		this.sizes = this.experience.sizes;
 		this.resources = this.experience.resources;
 		this.time = this.experience.time;
 		this.camera = this.experience.camera;
+		this.room = this.experience.world.room.finalRoom;
+		GSAP.registerPlugin(ScrollTrigger);
+
+		this.setPath();
 
 		// this.progress = 0;
 		// this.position = new THREE.Vector3(0, 0, 0);
@@ -18,13 +25,30 @@ export default class Controls {
 		// this.crossVector = new THREE.Vector3(0, 0, 0);
 
 		// Linear interpolation, or “lerp” for short, is a technique commonly used when programming things like games or GUIs. In principle, a lerp function “eases” the transition between two values over time, using some simple math
-		this.lerp = {
-			current: 0,
-			target: 0,
-			ease: 0.1,
-		};
+		// this.lerp = {
+		// 	current: 0,
+		// 	target: 0,
+		// 	ease: 0.1,
+		// };
 	}
 
+	setPath() {
+		this.timeline = new GSAP.timeline();
+		this.timeline.to(this.room.position, {
+			//to update on resize a callback function is needed and invalidateOnRefresh needs to be true
+			x: () => {
+				return this.sizes.width * 0.0015;
+			},
+			scrollTrigger: {
+				trigger: ".first-move",
+				markers: true,
+				start: "top top",
+				end: "bottom bottom",
+				scrub: 0.6,
+				invalidateOnRefresh: true,
+			},
+		});
+	}
 	//USED TO CREATE PATH FOR CAMERA OR MANY OTHER THINGS
 	// setPath() {
 	// 	//generating a curve
@@ -63,27 +87,21 @@ export default class Controls {
 	// }
 
 	update() {
-		this.lerp.current = gsap.utils.interpolate(
-			this.lerp.current,
-			this.lerp.target,
-			this.lerp.ease
-		);
-
+		// this.lerp.current = gsap.utils.interpolate(
+		// 	this.lerp.current,
+		// 	this.lerp.target,
+		// 	this.lerp.ease
+		// );
 		//THIS SECTION HELPS IF CAMERA HAS TO LOOK IN OUTSIDE OR INSIDE DIRECTION
-
 		// this.curve.getPointAt(this.lerp.current % 1, this.position);
 		// this.curve.getPointAt(this.lerp.current + 0.0001, this.lookAtPosition);
 		// this.camera.orthographicCamera.position.copy(this.position);
-
 		// this.directionalVector.subVectors(this.lookAtPosition, this.position);
 		// this.directionalVector.normalize();
 		// this.crossVector.crossVectors(this.directionalVector, this.staticVector);
 		// this.crossVector.multiplyScalar(1000000);
-
 		// this.camera.orthographicCamera.lookAt(this.crossVector);
-
 		//THIS SECTION HELPS WHEN CAMERA IS LOOKING AT THE PATH ITSELF
-
 		// if (this.back) {
 		// 	this.lerp.target -= 0.0001;
 		// } else {
@@ -95,7 +113,6 @@ export default class Controls {
 		// this.curve.getPointAt(this.lerp.current, this.position);
 		// this.curve.getPointAt(this.lerp.current + 0.0001, this.lookAtPosition);
 		// // this.progress -= 0.001;
-
 		// this.camera.orthographicCamera.position.copy(this.position);
 		// this.camera.orthographicCamera.lookAt(this.lookAtPosition);
 	}
