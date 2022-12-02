339f682b8854f6e9d562ef5618d3ec9c7065df25
chiggy69 <chiragaggarwal69@gmail.com>
2022-12-03 01:24:50 +0530

    emits event depending upon width

--- Experience/Utils/Sizes.js
+++ Experience/Utils/Sizes.js
@@ -8,12 +8,26 @@ export default class Sizes extends EventEmitter {
 		this.pixelRatio = Math.min(window.devicePixelRatio, 2);
 		this.frustrum = 5;
 
+		if (this.width < 968) {
+			this.device = "mobile";
+		} else {
+			this.device = "desktop";
+		}
+
 		window.addEventListener("resize", () => {
 			this.width = window.innerWidth;
 			this.height = window.innerHeight;
 			// console.log(this.width, this.height);
 			this.aspect = this.width / this.height;
 			this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+
+			if (this.width < 968 && this.device != "mobile") {
+				this.device = "mobile";
+				this.emit("switchDevice", this.device);
+			} else if (this.width >= 968 && this.device != "desktop") {
+				this.device = "desktop";
+				this.emit("switchDevice", this.device);
+			}
 			this.emit("resize");
 		});
 	}
