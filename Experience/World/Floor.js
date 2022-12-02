ce4792c6e808fcf781bcfd29b32ea302625f19cf
chiggy69 <chiragaggarwal69@gmail.com>
2022-12-03 01:23:38 +0530

    set the circles under the room

--- Experience/World/Floor.js
+++ Experience/World/Floor.js
@@ -7,6 +7,37 @@ export default class Floor {
 		this.scene = this.experience.scene;
 
 		this.setFloor();
+		this.setCircles();
+	}
+
+	setCircles() {
+		const geometry = new THREE.CircleGeometry(5, 64);
+		const material1 = new THREE.MeshStandardMaterial({ color: "#e5a1aa" });
+		const material2 = new THREE.MeshStandardMaterial({ color: "#8395cd" });
+		const material3 = new THREE.MeshStandardMaterial({ color: "#7ad0ac" });
+		this.circle1 = new THREE.Mesh(geometry, material1);
+		this.circle2 = new THREE.Mesh(geometry, material2);
+		this.circle3 = new THREE.Mesh(geometry, material3);
+		this.circle1.position.y = -0.29;
+		this.circle2.position.y = -0.28;
+		this.circle2.position.x = 2;
+		this.circle3.position.y = -0.27;
+
+		this.circle1.scale.set(0, 0, 0);
+		this.circle2.scale.set(0, 0, 0);
+		this.circle3.scale.set(0, 0, 0);
+
+		this.circle1.rotation.x = -Math.PI / 2;
+		this.circle2.rotation.x = -Math.PI / 2;
+		this.circle3.rotation.x = -Math.PI / 2;
+
+		this.circle1.receiveShadow = true;
+		this.circle2.receiveShadow = true;
+		this.circle3.receiveShadow = true;
+
+		this.scene.add(this.circle1);
+		this.scene.add(this.circle2);
+		this.scene.add(this.circle3);
 	}
 
 	setFloor() {
