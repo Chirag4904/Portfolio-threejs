30e6eb95d9c76fb13b9225b31c44b6629effd89c
chiggy69 <chiragaggarwal69@gmail.com>
2022-12-03 01:23:16 +0530

    world extends eventemitter now

--- Experience/World/World.js
+++ Experience/World/World.js
@@ -1,11 +1,13 @@
 import Experience from "../Experience";
+import { EventEmitter } from "events";
 
 import Room from "./Room";
 import Environment from "./Environment";
 import Controls from "./Controls";
 import Floor from "./Floor";
-export default class World {
+export default class World extends EventEmitter {
 	constructor() {
+		super();
 		this.experience = new Experience();
 		this.scene = this.experience.scene;
 		this.resources = this.experience.resources;
@@ -13,8 +15,9 @@ export default class World {
 		this.resources.on("loaded", () => {
 			this.environment = new Environment();
 			this.room = new Room();
-			this.controls = new Controls();
 			this.floor = new Floor();
+			this.controls = new Controls();
+			this.emit("worldReady");
 		});
 
 		this.theme.on("switch", (theme) => {
