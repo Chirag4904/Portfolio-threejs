6bcde76ed899d34b945dbfd2dcef3ac415bbee3c
chiggy69 <chiragaggarwal69@gmail.com>
2022-12-13 02:01:53 +0530

    tween animation to load objects

--- Experience/World/Preloader.js
+++ Experience/World/Preloader.js
@@ -1,6 +1,6 @@
 import Experience from "../Experience";
 import { EventEmitter } from "events";
-import * as THREE from "three";
+
 import GSAP from "gsap";
 export default class Preloader extends EventEmitter {
 	constructor() {
@@ -67,79 +67,235 @@ export default class Preloader extends EventEmitter {
 	secondIntro() {
 		return new Promise((resolve) => {
 			this.secondTimeline = new GSAP.timeline();
-			if (this.device === "desktop") {
-				this.secondTimeline
-					.to(
-						this.room.position,
-						{
-							x: 0,
-							y: 0,
-							z: 0,
-							ease: "power1.out",
-						},
-						"<"
-					)
-					.to(
-						this.roomChildren.cube.rotation,
-						{
-							y: 2 * Math.PI + Math.PI / 4,
-						},
-						"<"
-					)
-					.to(
-						this.roomChildren.cube.scale,
-						{
-							x: 10,
-							y: 10,
-							z: 10,
-						},
-						"<"
-					)
-					.to(
-						this.camera.orthographicCamera.position,
-						{
-							y: 4,
-						},
-						"<"
-					)
-					.to(
-						this.roomChildren.cube.position,
-						{
-							x: 0.638711,
-							y: 8.5618,
-							z: 1.3243,
-						},
-						"<"
-					)
-					.set(this.roomChildren.body.scale, {
-						x: 1,
-						y: 1,
-						z: 1,
-					});
-			} else {
-				this.secondTimeline.to(this.room.position, {
+
+			this.secondTimeline
+				.to(
+					this.room.position,
+					{
+						x: 0,
+						y: 0,
+						z: 0,
+						ease: "power1.out",
+					},
+					"<"
+				)
+				.to(
+					this.roomChildren.cube.rotation,
+					{
+						y: 2 * Math.PI + Math.PI / 4,
+					},
+					"<"
+				)
+				.to(
+					this.roomChildren.cube.scale,
+					{
+						x: 10,
+						y: 10,
+						z: 10,
+					},
+					"<"
+				)
+				.to(
+					this.camera.orthographicCamera.position,
+					{
+						y: 4,
+					},
+					"<"
+				)
+				.to(
+					this.roomChildren.cube.position,
+					{
+						x: 0.638711,
+						y: 8.5618,
+						z: 1.3243,
+					},
+					"<"
+				)
+				.set(this.roomChildren.body.scale, {
+					x: 1,
+					y: 1,
+					z: 1,
+				})
+				.to(this.roomChildren.cube.scale, {
 					x: 0,
 					y: 0,
 					z: 0,
-					ease: "power1.out",
-					duration: 0.7,
-				});
-			}
+					duration: 1,
+				})
+				.to(
+					this.roomChildren.aquarium.scale,
+					{
+						x: 1,
+						y: 1,
+						z: 1,
+						ease: "back.out(2.2)",
+						duration: 0.5,
+					},
+					">-0.5"
+				)
+				.to(
+					this.roomChildren.rectLight.scale,
+					{
+						x: 1,
+						y: 1,
+						z: 1,
+						ease: "back.out(2.2)",
+						duration: 1.5,
+					},
+					">-0.2"
+				)
+				.to(
+					this.roomChildren.clock.scale,
+					{
+						x: 1,
+						y: 1,
+						z: 1,
+						ease: "back.out(2.2)",
+						duration: 0.5,
+					},
+					">-0.4"
+				)
+				.to(
+					this.roomChildren.shelves.scale,
+					{
+						x: 1,
+						y: 1,
+						z: 1,
+						ease: "back.out(2.2)",
+						duration: 0.5,
+					},
+					">-0.3"
+				)
+				.to(
+					this.roomChildren.lampLight,
+					{
+						intensity: 1.4,
+						ease: "back.out(2.2)",
+						duration: 0.5,
+					},
+					">-0.3"
+				)
+				.to(
+					this.roomChildren.floor_items.scale,
+					{
+						x: 1,
+						y: 1,
+						z: 1,
+						ease: "back.out(2.2)",
+						duration: 0.5,
+					},
+					">-0.2"
+				)
+				.to(
+					this.roomChildren.desks.scale,
+					{
+						x: 1,
+						y: 1,
+						z: 1,
+						ease: "back.out(2.2)",
+						duration: 0.5,
+					},
+					">-0.1"
+				)
+				.to(
+					this.roomChildren.table_stuff.scale,
+					{
+						x: 1,
+						y: 1,
+						z: 1,
+						ease: "back.out(2.2)",
+						duration: 0.5,
+					},
+					">-0.1"
+				)
+				.to(this.roomChildren.computer.scale, {
+					x: 1,
+					y: 1,
+					z: 1,
+					ease: "back.out(2.2)",
+					duration: 0.5,
+				})
+				.set(this.roomChildren.mini_floor.scale, {
+					x: 1,
+					y: 1,
+					z: 1,
+				})
+				.to(this.roomChildren.chair.scale, {
+					x: 1,
+					y: 1,
+					z: 1,
+					ease: "back.out(2.2)",
+					duration: 0.5,
+				})
+				.to(
+					this.roomChildren.fish.scale,
+					{
+						x: 1,
+						y: 1,
+						z: 1,
+						ease: "back.out(2.2)",
+						duration: 0.5,
+					},
+					"<"
+				)
+				.to(
+					this.roomChildren.chair.rotation,
+					{
+						y: 4 * Math.PI + Math.PI / 4,
+						ease: "power2.out",
+
+						onComplete: resolve,
+						duration: 1,
+					},
+					"<"
+				);
 		});
+	}
+
+	removeEventListeners() {
+		window.removeEventListener("wheel", this.scrollOnceEvent);
+		window.removeEventListener("touchstart", this.touchStart);
+		window.removeEventListener("touchmove", this.touchMove);
 	}
 
 	onScroll(e) {
 		if (e.deltaY > 0) {
 			// console.log("first");
-			window.removeEventListener("wheel", this.scrollOnceEvent);
-			this.secondIntro();
+			this.removeEventListeners();
+			this.playSecondIntro();
 		}
+	}
+
+	onTouch(e) {
+		this.initalY = e.touches[0].clientY;
+	}
+
+	onTouchMove(e) {
+		let currentY = e.touches[0].clientY;
+		let difference = this.initalY - currentY;
+		if (difference > 0) {
+			console.log("swipped up");
+			this.removeEventListeners();
+			this.playSecondIntro();
+		}
+		this.intialY = null;
 	}
 
 	async playIntro() {
 		//wait for the first animation to play first
 		await this.firstIntro();
 		this.scrollOnceEvent = this.onScroll.bind(this);
+
+		this.touchStart = this.onTouch.bind(this);
+		this.touchMove = this.onTouchMove.bind(this);
 		window.addEventListener("wheel", this.scrollOnceEvent);
+		window.addEventListener("touchstart", this.touchStart);
+		window.addEventListener("touchmove", this.touchMove);
+	}
+
+	async playSecondIntro() {
+		await this.secondIntro();
+		console.log(this.roomChildren.lampLight);
+		this.emit("enableControls");
 	}
 }
